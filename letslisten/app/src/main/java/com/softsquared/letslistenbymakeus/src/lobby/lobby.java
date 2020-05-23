c858f9d4481145e54cd1737931740be93c3d1425
rockdam <choi37338@naver.com>
2020-05-23 10:04:20 +0900

    스플래시 엑티비티 작성

---
+++ letslisten/app/src/main/java/com/softsquared/letslistenbymakeus/src/lobby/lobby.java
@@ -0,0 +1,14 @@
+package com.softsquared.letslistenbymakeus.src.lobby;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class lobby extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lobby);
+    }
+}
